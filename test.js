bc083bdf547398804bb416c6fd8c61703193b289
AlexMoss <moss.alexr@gmail.com>
2022-07-25 16:53:21 -0400

    push

---
+++ test.js
@@ -0,0 +1,24 @@
+alert("Hello, World!");
+
+var myVariable;
+myVariable = 10;
+var myVar2 = "hello";
+myVar2 = "hello again";
+myVar2 = 5;
+var customerAge = 32;
+var myVar3 =40;
+var myVar4 = 10;
+myVar4 = myVar4 +5;
+myVar4 += 5;
+myVar4 /= 5;
+myVar4 *= 5;
+myVar4 +=1;
+myVar4++;
+myVar5 = "alex";
+document.write(myVar5);
+console.log(myVar5);
+document.write(10);
+console.log(50)
+console.log(20*5)
+console.write(20/5)
+
